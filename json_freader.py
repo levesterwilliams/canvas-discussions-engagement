c8b2fced6c34512e2f311362919098462343952b
Levester Williams <levesterwilliams@gmail.com>
2024-10-14 20:23:03 -0400

    Remove logging error library and statements.

--- json_freader.py
+++ json_freader.py
@@ -7,12 +7,6 @@
 #
 
 import json
-import logging
-
-# Set up logging
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.ERROR)
-
 
 class JSONfreader:
     def __init__(self):
@@ -27,12 +21,12 @@ class JSONfreader:
             json_file (str): Path to the JSON file
 
         Returns:
-            dict: Dictionary containing the loaded credentials, or None if an error occurs.
+            dict: Dictionary containing the loaded credentials, or None if an
+            error occurs.
 
         Notes:
-            If error occurs in opening the JSON file, the function will log
-            an error message and raise a runtime error exception for
-            client/caller to handle.
+            If error occurs in opening the JSON file, the function raise a
+            error exception for client/caller to handle.
         """
 
         try:
@@ -40,12 +34,9 @@ class JSONfreader:
                 self._credentials = json.load(file)
                 return self._credentials
         except FileNotFoundError as e:
-            logger.error(f"File not found::{e}.")
-            raise RuntimeError(
+            raise FileNotFoundError(
                 "Failed to load credentials due to missing file.") from e
         except json.JSONDecodeError as e:
-            logger.error(f"Invalid JSON::{e}.")
             raise RuntimeError("The JSON file contains invalid JSON") from e
         except Exception as e:
-            logger.error(f"Error loading credentials::{e}.")
             raise RuntimeError("Error in loading credentials") from e
